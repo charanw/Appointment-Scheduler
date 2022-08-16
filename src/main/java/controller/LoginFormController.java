6311d47498fbf80e84e0e963c078a130ca8b6376
Charan Williams <charanwilliams@gmail.com>
2022-08-15 22:34:08 -0500

    Refactored password checking Added login audit logs

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import com.example.model.main;
 import helper.controller;
 import helper.userQuery;
 import javafx.event.ActionEvent;
@@ -8,8 +9,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 
 public class LoginFormController extends controller {
@@ -38,6 +45,13 @@ public class LoginFormController extends controller {
     @FXML
     private Label userNameLabel;
 
+    public void printToFile(String userName, String loginType, Timestamp timestamp) throws IOException {
+        FileWriter fileWriter = new FileWriter("login_activity.txt", true);
+        PrintWriter printer = new PrintWriter(fileWriter);
+        printer.println(loginType + " login by " +userName + " at " + timestamp);
+        printer.close();
+    }
+
     @FXML
     void exitButtonClicked(ActionEvent event) {
     System.exit(0);
@@ -45,16 +59,20 @@ public class LoginFormController extends controller {
 
     @FXML
     void loginButtonClicked(ActionEvent event) throws IOException, SQLException {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         String userName = userNameField.getText();
         String password = passwordField.getText();
         if(userQuery.lookupUsername(userName)) {
-            if (Objects.equals(password, userQuery.getPassword(userName))) {
+            if (userQuery.checkPassword(userName,password)) {
                 changeScene(event, "/com/example/model/MainMenu.fxml");
+                printToFile(userName, "Successful", timestamp);
             } else {
                 error("Invalid password. PLease try again.");
+                printToFile(userName, "Unsuccessful", timestamp);
             }
         } else {
             error("Invalid username. Please try again.");
+            printToFile(userName, "Unsuccessful", timestamp);
         }
     }
 }
--- src/main/java/helper/userQuery.java
+++ src/main/java/helper/userQuery.java
@@ -11,22 +11,24 @@ public class userQuery {
         String sql = "SELECT User_Name FROM users";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
-        while(rs.next()) {
+        while (rs.next()) {
             if (Objects.equals(userName, rs.getString("User_Name"))){
                 return true;
             }
         }
         return false;
     };
-    public static String getPassword(String userName) throws SQLException {
+    public static boolean checkPassword(String userName, String password) throws SQLException {
         String sql = "SELECT Password FROM users WHERE User_Name = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ps.setString(1, userName);
         ResultSet rs = ps.executeQuery();
-        String password = null;
-        if(rs.next()) {
-        password = rs.getString("Password");
+
+        if (rs.next()) {
+        if (Objects.equals(password, rs.getString("Password"))) {
+            return true;
         }
-        return password;
+        }
+        return false;
     };
 }
